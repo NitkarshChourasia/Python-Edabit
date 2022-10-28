6862370f65695332b874760b675130505f9be83b
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2022-10-28 22:57:59 +0530

    #2 Program Solved Successfully.

--- Find the Largest Number in a List.py
+++ Find the Largest Number in a List.py
@@ -1,4 +1,4 @@
-def findlargestNum():
+def findlargestNummine():
     arrsize = int(input('Please input array size:\n'))
     i = 1
     list1 = []
@@ -11,11 +11,26 @@ def findlargestNum():
         list1.append(listinput1)
         i = i + 1
     print(list1)
-findlargestNum()
+    print("Largest number among it is: {}".format(max(list1)))
+    """Another way to find the largest number in the list is:-
+        list1.sort()
+        print(list1[-1})
+    """
+    list1.sort()
+    print('Sort function way to find the largest number in the list: {}'.format(list1[-1]))
+# findlargestNummine()
 
 # To test whether a function can run it's own inherited functions?
 
-
+######### Main Program starts here. ##############
 # edabit submitted solution:
 def findLargestNum(nums):
 	return(max(nums))
+
+######## Main Program ends here. ##################
+# Program lines after this were not submitted to the edabit.
+# It is for the user to print the outputs, for myself I mean.
+b = findLargestNum([5,64,5646,564])
+print(b)
+# a = findlargestNummine()
+# print(a)
