9c5c6f12195ca6253872df93233b4c810753804f
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2022-10-29 00:57:29 +0530

    #12 Program Solved Successfully.

---
+++ Find the Perimeter of a Rectangle.py
@@ -0,0 +1,16 @@
+# Edubit submitted solution.
+# Main program starts here.
+def findPerimeter(length, width):
+    return ((length * 2) + (width * 2))
+# Main program ends here.
+# My code after this.
+
+print(findPerimeter(1,2))
+print(findPerimeter(2,4))
+print(findPerimeter(3,6))
+print(findPerimeter(5,10))
+print(findPerimeter(10,20))
+
+
+# Should revisit all my codes and see what improvements I can make in them.
+# Should revisit all my codes and see what new programs I can make out of them.
