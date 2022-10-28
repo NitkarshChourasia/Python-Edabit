82fa51cf3fb0fb4a5b4f3af9f3fe90e66f61c584
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2022-10-28 23:14:52 +0530

    #4 Program Solved Successfully.

---
+++ Return the Next Number from the Integer Passed.py
@@ -0,0 +1,9 @@
+# Edabit Submitted Solution:
+####### Main program starts here. #######
+def addition(num):
+    return num+1
+####### Main program ends here. ######
+# Program lines after this were not submitted to the edabit.
+# It is for the user to print the outputs, for myself I mean.
+print(addition(0))
+print(addition(1))
