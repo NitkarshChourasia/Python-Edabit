6064ea1695ecb40cdeda9b376cfa3f85fc4fcdd2
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2022-10-29 00:32:38 +0530

    #9 Program Solved Successfully.

---
+++ Maximum Edge of a Triangle.py
@@ -0,0 +1,9 @@
+# Edabit submitted solution:
+# Main program starts here.
+def nextEdge(side1, side2):
+    return (side1 + side2) - 1
+# Main program ends here.
+# My code after this.
+print(nextEdge(5,5))
+print(nextEdge(10,5))
+print(nextEdge(2,4))
