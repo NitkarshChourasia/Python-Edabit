20780fe476bb1c3727f83ba637a26a6fe3cf645b
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2022-10-29 00:38:55 +0530

    #10 Program Solved Successfully.

---
+++ buggyCodeSyntaxError.py
@@ -0,0 +1,22 @@
+# Edabit submitted solution.
+# Main program starts here.
+def cubes(a):
+    # retur a**3
+    # Syntax error in edubit problem
+    # Solution submitted be me.
+    return a ** 3
+# Main program ends here.
+# My code after this.
+
+print(cubes(1))
+print(cubes(2))
+print(cubes(3))
+print(cubes(4))
+print(cubes(5))
+print(cubes(6))
+print(cubes(7))
+
+# Should make a program out of this to print cubes table.
+# Same program I should make using the square program.
+# Like it will make 2-3 programs out of one concept.
+
