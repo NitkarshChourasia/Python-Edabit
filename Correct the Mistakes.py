77f14c280e515914ff8a48ccc794397920f81bd6
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2022-10-29 00:26:13 +0530

    #8 Program Solved Successfully.

---
+++ Correct the Mistakes.py
@@ -0,0 +1,11 @@
+# Edabit submitted solution
+# Main program starts here.
+
+# def squead(b):
+# Corrected to:-
+def squared(a):
+    return a * a
+# My program after this
+print('Square of {} is {}.'.format(5,squared(5)))
+print('Square of {} is {}.'.format(9,squared(9)))
+print('Square of {} is {}.'.format(6,squared(6)))
