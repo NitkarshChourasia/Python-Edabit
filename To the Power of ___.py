1a0e8a2cf49282570c8d543b54120de0926d0b86
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2022-10-28 21:00:32 +0530

    First Program Commit

---
+++ To the Power of ___.py
@@ -0,0 +1,9 @@
+def calculate_exponent(num, exp):
+    i = 1
+    while ( i <= exp):
+        output1 = num*num*(i)
+        print(output1)
+        print(i)
+        i = i + 1
+        while (i = 0)
+calculate_exponent(2, 5)
