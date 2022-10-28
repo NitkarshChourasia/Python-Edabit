a6f9b399291a4c6592684b0a76fbd1b3292922ad
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2022-10-29 00:16:19 +0530

    #7 Program Solved Successfully.

---
+++ Area of a Triangle.py
@@ -0,0 +1,9 @@
+# Edabit submitted solution:
+# Main program starts here.
+def tri_area(base,height):
+    return(base*height/2)
+# Main program ends here.
+# My code after this.
+print(int(tri_area(10,20)))
+# 10 * 20 = 200.
+# So 200\2 = 100.
