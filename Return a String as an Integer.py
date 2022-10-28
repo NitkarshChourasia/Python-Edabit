6e55b9d562a357a431c6e712dee99e98a265e336
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2022-10-28 23:09:48 +0530

    #3 Program Solved Successfully.

---
+++ Return a String as an Integer.py
@@ -0,0 +1,12 @@
+# Edabit Submitted Solution:
+####### Main program starts here. #######
+def string_int(txt):
+    return int(txt)
+####### Main program ends here. ######
+# Program lines after this were not submitted to the edabit.
+# It is for the user to print the outputs, for myself I mean.
+a = string_int('42342')
+print(type(a))
+print(string_int('5454'))
+print(type(string_int('23542')))
+# So, it for sure converts string into integer, respectfully so.
