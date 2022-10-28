f47f98a2e8c2a63a4f5b1a4e3584fd237d3d205a
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2022-10-28 23:27:06 +0530

    #5 Program Solved Successfully.

---
+++ Convert Minutes into Seconds.py
@@ -0,0 +1,9 @@
+# Edabit Submitted Solution:
+####### Main program starts here. #######
+def convert(minutes):
+    return minutes*60
+####### Main program ends here. ######
+# Program lines after this were not submitted to the edabit.
+# It is for the user to print the outputs, for myself I mean.
+minutes = 5
+print('{} minutes being converted to seconds is:\n{} seconds'.format(minutes,convert(minutes)))
