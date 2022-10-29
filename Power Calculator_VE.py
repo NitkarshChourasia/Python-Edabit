ab02e3e670c5c3e27796bc13efc433fca0c402ba
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2022-10-29 09:49:48 +0530

    #17 Program Solved Successfully.

---
+++ Power Calculator_VE.py
@@ -0,0 +1,13 @@
+# Edabit submitted solution.
+# Main program starts here.
+def powerCalc(voltage,current):
+    return (voltage*current)
+# Main program ends here.
+# My code after this.
+print(powerCalc(230,10))
+print(powerCalc(110,3))
+print(powerCalc(480,20))
+
+# Should somehow improve this you know.
+# How and what to add but??
+# Hmmm?
