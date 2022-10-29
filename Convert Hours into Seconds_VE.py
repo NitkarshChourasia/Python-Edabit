15eea77d02c279ab37090c682edc766212dbe3fb
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2022-10-29 08:32:43 +0530

    #15 Program Solved Successfully.

---
+++ Convert Hours into Seconds_VE.py
@@ -0,0 +1,12 @@
+# Edubit submitted solution.
+# Main program starts here.
+def convert(hours):
+    return ( hours * 60 * 60)
+# Main program ends here.
+# My code after this.
+print(convert(24))
+print(convert(2))
+print(convert(10))
+
+# Afterwards will make my adjustments to it.
+# For sure.
