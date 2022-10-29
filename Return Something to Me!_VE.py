e64b3245a5721a53d6cd286e4b9fd473b49dca53
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2022-10-30 00:43:43 +0530

    #22 Program Solved Successfully.

---
+++ Return Something to Me!_VE.py
@@ -0,0 +1,15 @@
+# Submitted Solution.
+# Main program starts here.
+def returnSomething(a):
+    return 'Something' + ' ' + a
+# Main program ends here.
+# My code after this.
+
+a = 'is better than nothing.'
+print(returnSomething(a))
+
+a = 'Nitkarsh Chourasia.'
+print(returnSomething(a))
+
+a = 'Something.'
+print(returnSomething(a))
