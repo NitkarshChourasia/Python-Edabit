3080549c67a434cc83533824dc85bec1d734a95f
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2022-10-30 01:45:13 +0530

    #25 Program Solved Successfully.

---
+++ Is the Number Less than or Equal to Zero?_VE.py
@@ -0,0 +1,17 @@
+# Submitted Solution.
+# Main program starts here.
+
+def lessThanOrEqualZero(input1):
+    print(True) if input1 <= 0 else print(False)
+
+# Main program ends here.
+# My code after this.
+
+lessThanOrEqualZero(5)
+lessThanOrEqualZero(0)
+lessThanOrEqualZero(-2)
+
+# My code ends here.
+# Program runs as expected.
+# Congratulations on that, Nitkarsh Chourasia.
+# Well done.
