6c0674ae782d8e7cdc9f979671f39fc0dabd77ad
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2022-10-30 01:40:22 +0530

    #24 Program Solved Successfully.

---
+++ Are the Numbers Equal?_VE.py
@@ -0,0 +1,21 @@
+# Submitted Program.
+# Main program starts here.
+
+def numEqual(num1, num2):
+    return (print(True) if num1 == num2 else print(False))
+    # if num1 == num2:
+    #     print(True)
+    # else:
+    #     print(False)
+# Main program ends here.
+# My code after this.
+numEqual(4,8)
+numEqual(2,2)
+numEqual(2, '2')
+# Everything is right but the line outputs None after every bool? Why?
+
+# Let's try without Short Hand If ...Else.
+# Tried short hand if..else too.
+# Both works the same as expected.
+# Congratulations on that.
+# My code ends here.
