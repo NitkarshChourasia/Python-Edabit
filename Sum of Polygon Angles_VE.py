87b0c8ad11393a0aab4f556bd1ec4fadf944cc60
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2022-10-29 12:14:39 +0530

    #18 Program Solved Successfully.

---
+++ Sum of Polygon Angles_VE.py
@@ -0,0 +1,9 @@
+# Edabit submitted solution.
+# Main program starts here.
+def sumOfPolygon(n):
+    return((n-2)*(180))
+# Main program ends here.
+# My code after this.
+print(sumOfPolygon(3))
+print(sumOfPolygon(4))
+print(sumOfPolygon(6))
