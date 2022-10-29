17960696962a5974ee88cd3e857d2d544a9d3470
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2022-10-29 12:40:03 +0530

    #20 Program Solved Successfully.

---
+++ The Farm Problem_VE.py
@@ -0,0 +1,8 @@
+# Main program starts here.
+def totalLegs(chickens, cows, pigs):
+    return ((chickens*2)+(cows*4)+(pigs*4))
+# Main program ends here.
+# My code after this.
+print(totalLegs(2,3,5))
+print(totalLegs(1,2,8))
+print(totalLegs(1,2,3))
