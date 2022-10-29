f8ded45949fdb5ee0473c8b9cc14ef255cddef00
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2022-10-29 07:44:44 +0530

    #13 Program Solved Successfully.

---
+++ Return the Sum of Two Numbers.py
@@ -0,0 +1,13 @@
+# Edabit Submitted Solution:
+# Main program starts here.
+def addition (a, b):
+    return a + b
+# Main program ends here.
+# My code after this.
+print(addition(2,2))
+print(addition(4,4))
+print(addition(3,5))
+print(addition(44,4))
+print(addition(6,6))
+print(addition(23,42))
+print(addition(32,232))
