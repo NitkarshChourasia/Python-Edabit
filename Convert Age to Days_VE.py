3ac45825b87d62dda7c0a29fbec718aff5295ee8
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2022-10-29 09:45:47 +0530

    #16 Program Solved Successfully.

---
+++ Convert Age to Days_VE.py
@@ -0,0 +1,16 @@
+# Edabit submitted solution:
+# Main program starts here.
+def convert(age):
+    return(age*365)
+# Main program ends here.
+# My code after this.
+print(convert(10))
+print(convert(20))
+print(convert(30))
+print(convert(40))
+print(convert(50))
+
+# Funtions I need to add further are.
+# leap years and days between last birthday and now.
+# Expecting only positive integer input.
+
