dc07ee65bd810cd7fcf3e71304f972be988f5f80
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2022-10-30 00:58:37 +0530

    #23 Program Solved Successfully.

---
+++ Basketball Points_VE.py
@@ -0,0 +1,14 @@
+# Submitted Solution.
+# Main program starts from here.
+
+def basketballPoints(pointer2, pointer3):
+    return((2*pointer2)+(3*pointer3))
+
+# Main program ends here.
+# My code after this.
+
+print(basketballPoints(1,1))
+print(basketballPoints(7,5))
+print(basketballPoints(38,8))
+
+# My code ends here.
