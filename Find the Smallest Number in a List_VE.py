bf7bee35f13ceed61911ece2781e24ef479bab45
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2022-10-30 07:44:40 +0530

    #26 Program Solved Successfully.

---
+++ Find the Smallest Number in a List_VE.py
@@ -0,0 +1,24 @@
+# Submitted Solution.
+# Main program starts here.
+def smallestNumber(list1):
+    return min(list1)
+
+# Main program ends here.
+# My code after this.
+
+print(smallestNumber([34, 15, 88, 2]))
+
+print(smallestNumber([34, -345, -1, 100]))
+
+print(smallestNumber([-76, 1.345, 1, 0]))
+
+print(smallestNumber([0.4356, 0.8795, 0.5435, -0.9999]))
+
+print(smallestNumber([7, 7, 7]))
+
+# My code ends here.
+# Works as expected.
+# Good...
+# Keep up the good work.
+# Awesome.
+# Congratulations, Nitkarsh Chourasia.
