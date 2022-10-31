fc034b9bcc76b53543871e73b28d413bfa98d441
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2022-10-31 15:18:17 +0530

    #28 Program Solved Successfully.

---
+++ Proper Modulo Operator_VE.py
@@ -0,0 +1,15 @@
+# Submitted Solution.
+# Main program starts here.
+def mod(m,n):
+    return m%n
+
+# Main program ends here.
+# My code after this.
+# Test cases here.
+
+print(mod(-13,64))
+print(mod(50,25))
+print(mod(-6,3))
+
+# All test cases werer successful.
+# Congratulations, the code was correct.
